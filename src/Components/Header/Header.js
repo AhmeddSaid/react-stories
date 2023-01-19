e7609c9a728cc9111f8251827c9e9399bd5a4163
Ahmed Said <ahmed_adnan@stud.cu.edu.eg>
2023-01-19 02:57:29 +0200

    first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
       <div className="title">Story</div>
       <ul className="nav">
         <li>
-          <a href="/#">Menu</a>
+          <a href="/">Menu</a>
         </li>
         <li>
-          <a href="/#">About</a>
+          <a href="/">About</a>
         </li>
       </ul>
     </div>
